e99925ff01b35ee59ee303152bf35a134bac7b1a
Tung Nguyen <tnguyen52@mantu.com>
2024-08-24 15:35:45 +0700

    Initialize empty client app

---
+++ src/client-app/src/layouts/MainLayout.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-layout view="hHh Lpr lff">
+    <q-header elevated :class="$q.dark.isActive ? 'bg-secondary' : 'bg-black'">
+      <q-toolbar>
+        <q-btn flat @click="toggleLeftDrawer" round dense icon="menu" />
+        <q-toolbar-title>CEO Rank</q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item to="/" exact>
+          <q-item-section avatar>
+            <q-icon name="home" />
+          </q-item-section>
+          <q-item-section> Home </q-item-section>
+        </q-item>
+
+        <q-item to="/watchlist" exact>
+          <q-item-section avatar>
+            <q-icon name="star" />
+          </q-item-section>
+          <q-item-section> Watchlist </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+defineOptions({
+  name: 'MainLayout',
+});
+
+const leftDrawerOpen = ref(false);
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+</script>
---
+++ src/client-app/src/pages/WatchlistPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <h3>Watchlist</h3>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: 'WatchlistPage',
+});
+</script>
